e5b9e901e2c10e31a903b9c880d093b2dc522bea
Nick Perry <nick.perry12@outlook.com>
2024-07-17 15:33:23 -0700

    added prod build

--- index.js
+++ index.js
@@ -108,6 +108,6 @@ app.post('/api/contacts', (req, res) => {
   res.json(contact);
 });
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 app.listen(PORT);
 console.log(`Server running on ${PORT}`);
