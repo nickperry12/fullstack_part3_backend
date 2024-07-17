970885ef25f184c03d51318657b0310e3b424a84
Nick Perry <nick.perry12@outlook.com>
2024-07-17 15:32:08 -0700

    added prod build

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(express.json());
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`));
 app.use(cors());
+app.use(express.static('dist'));
 
 let contacts = [
   { 
